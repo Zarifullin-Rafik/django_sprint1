eb60ef781711e7b184ccd5128d9ec432da335889
Zarifullin-Rafik <150954447+Zarifullin-Rafik@users.noreply.github.com>
2024-03-19 01:06:52 +0300

    Second

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -46,18 +46,18 @@ posts = [
 
 
 def index(request):
-    template = 'blog/index.html' 
+    template = 'blog/index.html'
     context = {'posts': reversed(posts)}
-    return render(request, template, context) 
+    return render(request, template, context)
 
 
-def post_detail(request, id): 
+def post_detail(request, id):
     template = 'blog/detail.html'
     context = {'post': posts[id]}
     return render(request, template, context)
 
 
-def category_posts(request, category_slug): 
+def category_posts(request, category_slug):
     template = 'blog/category.html'
     context = {'category_slug': category_slug}
     return render(request, template, context)
